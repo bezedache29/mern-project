989a7c6d8ae8d5683f9531e0a0c2490482899982
Tophe <bezedache29@gmail.com>
2021-12-08 21:29:43 +0100

    Init du server + installation express + Installation et mongoose

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose
+  .connect('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.vzt8o.mongodb.net/mern-project', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((e) => console.log('Failed to connect to MongoDB', e))
